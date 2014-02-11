dfb2c2df994ad4e3f32fc8eba0cdd1e50f9f7f08
Jalouzot <jjalouzot@eleven-labs.com>
2014-02-11 22:39:23 +0100

    Add mocha test

---
+++ test/model-tasks-tests.js
@@ -0,0 +1,15 @@
+define(function(require) {
+  var models = require('taskmodel');
+ 
+  describe('Models', function() {
+ 
+    describe('Sample Model', function() {
+      it('should default "urlRoot" property to "/api/samples"', function() {
+        var sample = new models.Sample();
+        sample.urlRoot.should.equal('/api/samples');
+      });
+    });
+ 
+  });
+ 
+});
