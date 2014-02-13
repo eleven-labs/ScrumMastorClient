3a573e18a6d9477632fc1b6b6b1d05f090981251
Jalouzot <jjalouzot@eleven-labs.com>
2014-02-13 22:23:05 +0100

    Add test for model

--- test/modelTaskSpec.js
+++ test/modelTaskSpec.js
@@ -4,11 +4,24 @@ define(['taskmodel', 'chai'], function(TaskModel, chai)  {
 
   describe('TaskModel', function() {
 
-    it('Test setTitle', function() {
-    	var task = TaskModel();
+    it('Test setTitle and getTitle', function() {
+    	var task = new TaskModel();
     	task.setTitle('Test')
         
         chai.expect(task.getTitle()).to.equal('Test');
+    });
+
+    it('Test setTitle empty', function() {
+    	var task = new TaskModel();
+    	
+        chai.expect(task.setTitle('')).to.equal('Le titre du document ne peut pas être vide !!!');
+    });
+
+    it('Test setDescription and getDescription', function() {
+    	var task = new TaskModel();
+    	task.setDescription('Test')
+        
+        chai.expect(task.getDescription()).to.equal('Test');
     });
 
   });
