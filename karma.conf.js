c838f3d7de9f8fea3d5c004cc697c0b22c07fbbe
Jalouzot <jjalouzot@eleven-labs.com>
2014-03-09 17:55:37 +0100

    Change display

--- karma.conf.js
+++ karma.conf.js
@@ -8,7 +8,7 @@ module.exports = function(config) {
     // list of files / patterns to load in the browser
     files: [
       'test/test-main.js',
-      'lib/tests//mock-ajax.js',
+      'lib/tests/mock-ajax.js',
       'lib/jquery/jquery.min.js',
       'lib/tests/jasmine-jquery.js',
 
