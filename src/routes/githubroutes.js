1215477fe96b8a53038d078b24cc35f8986b0c4e
Jalouzot <jjalouzot@eleven-labs.com>
2014-04-29 21:46:05 +0200

    Add url github

--- src/routes/githubroutes.js
+++ src/routes/githubroutes.js
@@ -12,7 +12,7 @@ define(['backbone', 'jquery'], function(Backbone, $) {
                 data: "client_id=bfcda35e836f13ee9d72&client_secret=4781cce7a55a180ed3ad20eeb6552562712fe0fa&code=" + code,
                 success: function(msg) {
                     console.log(msg);
-                }; 
+                },
             });
         },
     });
