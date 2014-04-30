4eac72bcb37dfffaae5b79d58dc550946d5732a0
Jalouzot <jjalouzot@eleven-labs.com>
2014-04-30 14:30:22 +0200

    add directly in prod

--- src/routes/githubroutes.js
+++ src/routes/githubroutes.js
@@ -6,15 +6,21 @@ define(['backbone', 'jquery'], function(Backbone, $) {
         },
 
         sendToken: function(code) {
-            $.ajax({
+        $.ajax({
                 type: "post",
+		crossDomain: true,
                 url: "https://github.com/login/oauth/access_token",
-                data: "client_id=bfcda35e836f13ee9d72&client_secret=4781cce7a55a180ed3ad20eeb6552562712fe0fa&code=" + code,
-                success: function(msg) {
-                    console.log(msg);
-                },
-            });
-        },
+                data: "client_id=bfcda35e836f13ee9d72&client_secret=4781cce7a55a180ed3ad20eeb6552562712fe0fa&code="+code,
+		dataType: 'JSONP',                
+        	success: function(responseData, textStatus, jqXHR) 
+    {
+        console.log(responseData);
+    },
+    error: function (responseData, textStatus, errorThrown) 
+    {
+        console.warn(responseData, textStatus, errorThrown);
+    }});
+	},
     });
 
     return GitHubRoutes;
